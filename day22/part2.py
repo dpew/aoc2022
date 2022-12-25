2ea0e4b3ef02f73b568f2faa5916dda4c0df052f
Douglas Pew [Spectric Labs] <dpew@spectric.com>
2022-12-24 19:38:37 -0500

    day 22 construction

--- day22/part2.py
+++ day22/part2.py
@@ -27,10 +27,14 @@ DVALUE = {
     (0, -1): 3
 }
 
-SIDES = ((0, None, None), (1, None, None),
-         (None, 0, None), (None, 1, None),
-         (None, None, 0), (None, None, 1))
-
+SIDES = {
+          'X0': (0, None, None),
+          'X1': (1, None, None),
+          'Y0': (None, 0, None),
+          'Y1': (None, 1, None),
+          'Z0': (None, None, 0),
+          'Z1': (None, None, 1)
+        }
 
 class Side:
 
@@ -41,9 +45,55 @@ class Side:
         assert sum(i is not None for i in side) == 1
 
     def put(self, pos, val):
+        if pos in self.grid:
+            raise KeyError(f"Side: {self.side} pos={pos}")
         self.grid[pos] = val
 
-    
+    def __getitem__(self, pos):
+        return self.grid[pos]
+
+
+def rotateX(direc, deg):
+    vec = advent.pos_to_vector3(direc)
+    mul = advent.rotateX(deg)
+    return advent.vector3_to_pos(np.dot(vec, mul))
+
+def rotateY(direc, deg):
+    vec = advent.pos_to_vector3(direc)
+    mul = advent.rotateY(deg)
+    return advent.vector3_to_pos(np.dot(vec, mul))
+
+
+def rotateZ(direc, deg):
+    vec = advent.pos_to_vector3(direc)
+    mul = advent.rotateZ(deg)
+    return advent.vector3_to_pos(np.dot(vec, mul))
+
+def turn(side, vector, direct):
+        deg = 0
+        if direct == 'R':
+            deg = 90
+        elif direct == 'L':
+            deg = -90
+        else:
+            raise ValueError("BAD direction " + direct)
+
+        if side == (None, None, 1):
+            direction = rotateZ(vector, -deg)
+        elif side == (None, None, 0):
+            direction = rotateZ(vector, deg)
+        elif side == (None, 1, None):
+            direction = rotateY(vector, -deg)
+        elif side == (None, 0, None):
+            direction = rotateY(vector, deg)
+        elif side == (1, None, None):
+            direction = rotateX(vector, -deg)
+        elif side == (0, None, None):
+            direction = rotateX(vector, deg)
+        else:
+            raise ValueError("BAD side " + side)
+        return direction
+
 
 def translate_3d(pos: Tuple[int], size: int) -> Tuple[int]:
     '''
@@ -57,37 +107,75 @@ def translate_3d(pos: Tuple[int], size: int) -> Tuple[int]:
         >>> translate_3d((3, 7), 4)
         ((3, 0, 3), (None, 0, None))
         >>> translate_3d((-1, 7), 4)
-        ((0, 3, 3), (0, None, None))
+        ((0, 0, 3), (0, None, None))
         >>> translate_3d((-3, 7), 4)
-        ((0, 1, 3), (0, None, None))
+        ((0, 2, 3), (0, None, None))
     '''
-    sideX = math.floor(pos[0] / size) % 2
+    print(f't3d {pos}')
+    sideX = math.floor(pos[0] / size)
     sideY = math.floor(pos[1] / size)
     posX = pos[0] % size
-    posY = pos[1] % size
-    return ((posX, posY), (sideX, sideY))
+    nposY = pos[1] % size
+    nposX = size - posX - 1
+    posY = size - nposY - 1
+    
+    if sideY == 0:
+        if sideX == 0:
+            return ((posX, posY, 0), SIDES['Z0'])
+        if sideX == 1:
+            return ((1, posY, posX), SIDES['X1'])
+    if sideY == 1:
+        if sideX == 0:
+            return ((posX, 0, nposY), SIDES['Y0'])
+        if sideX == -1:
+            return ((0, nposX, nposY), SIDES['X0'])
+        if sideX == -2:
+            return ((nposX, 1, nposY), SIDES['Y1'])
+    if sideY == 2:
+        if sideX == 0:
+            return ((posX, nposY, 1), SIDES['Z1'])
+        if sideX == -1:
+            return ((0, nposY, posX), SIDES['X0'])
+        if sideX == -2:
+            return ((0, nposY, posX), SIDES['X0'])
+    if sideY == 2:
+        if sideX == 0:
+            return ((posX, nposY, 1), SIDES['Z1'])
+        if sideX == -1:
+            return ((0, nposY, posX), SIDES['X0'])
+        if sideX == 1:
+            return ((1, nposY, nposX), SIDES['X1'])
+    raise ValueError((sideX, sideY))
 
 class Grove(object):
 
     def __init__(self, cubewidth: int):
         self.grid = {}
-        for side in SIDES:
+        for side in SIDES.values():
             self.grid[side] = Side(side)
+        self.height = 0
         self.start = None
-        self.pos = None
-        self.direction = (1, 0)
+        self.pos = (0, 0, 0)
+        self.direction = (1, 0, 0)
         self.cubewidth = cubewidth
+        self.curside = SIDES['Z0']
 
+    def print(self):
+        for sname, sval in SIDES.items():
+            print(f"SIDE {sname}:")
+            side = self.grid[sval]
+            for y in range(self.cubewidth):
+                print(''.join(side[(x, y)] for x in range(self.cubewidth)))
+            print()
 
     def add_row(self, row: Iterator[str]):
         for x, d in enumerate(row):
             mypos = (x, self.height)
             if self.start is None and d == '.':
                 self.start = mypos
-                self.pos = self.start
-            if self.start:
+            if self.start and d != ' ':
                 relpos = advent.subpos(mypos, self.start)
-                p3, sd = translate_3d(relpos)
+                p3, sd = translate_3d(relpos, self.cubewidth)
                 self.grid[sd].put(p3, d)
 
         self.height += 1
@@ -98,7 +186,7 @@ class Grove(object):
                 return 0
             if x == 0:
                 return 1
-            if return -1
+            return -1
         def fnextpos(dimen, flopside, newflop):
             # dimension if the new side (flopside), so return the new flop value
             if dimen == flopside:
@@ -115,14 +203,17 @@ class Grove(object):
         nextpos = advent.addpos(self.pos, self.direction)
         nextdir = self.direction
         nextside = self.curside
-        for d in dimension:
+        for d in (0, 1, 2):
             if nextpos[d] >= self.cubewidth or nextpos[d] < 0:
-                nval = 0 if nextpos[d] < 0 else 1
+                nval = 1 if nextpos[d] < 0 else 0
                 nextdir = tuple(fdirect(c) for c in self.curside)
+                nextside = tuple(nval if i == d else None for i in range(3))
                 nextpos = tuple(fnextpos(c, d, nval) for c in range(3))
-                nextside = tuple(1 if i == d else None in i range(3))
+                print(f" curside={self.curside} direction={self.direction} pos={self.pos}")
+                print(f"nextside={nextside} nextdir={nextdir} nextpos={nextpos}")
 
         # If next position is not a wall
+        pprint.pprint(self.grid[nextside].grid)
         if self.grid[nextside][nextpos] != '#':
             self.curside = nextside
             self.direction = nextdir
@@ -136,29 +227,8 @@ class Grove(object):
                 self.step()
 
     def turn(self, direct: str):
-        deg = 0
-        if direct == 'R':
-            deg = 90
-        elif direct == 'L':
-            deg = -90
-        else:
-            raise ValueError("BAD direction " + direct)
-
-        if self.curside == (None, None, 1):
-            direction = rotateZ(self.direction, deg)
-        elif self.curside == (None, None, 0):
-            direction = rotateZ(self.direction, -deg)
-        elif self.curside == (None, 1, None):
-            direction = rotateY(self.direction, deg)
-        elif self.curside == (None, 0, None):
-            direction = rotateY(self.direction, -deg)
-        elif self.curside == (1, None, None):
-            direction = rotateX(self.direction, deg)
-        elif self.curside == (0, None, None):
-            direction = rotateX(self.direction, -deg)
-        else
-            raise ValueError("BAD side " + self.curside)
-        self.direction = direction
+        newdirect = turn(self.curside, self.direction, direct)
+        self.direction = newdirect
 
     @property
     def mypos(self):
@@ -201,12 +271,12 @@ def main():
                 grove.add_row(row)
         else:
             gmoves = True
+    #grove.print()
 
-    assert grove.mypos == (0, 0)
     for m in process_moves(moves):
         print(m)
         grove.move(m)
-        print(f"pos={grove.pos}, rpos={grove.mypos} dir={grove.direction}")
+        print(f"pos={grove.pos}, dir={grove.direction}")
 
 
     pos = grove.pos
